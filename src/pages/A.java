c39b642534ac5df534fd87cefe72846266e1d6fc
general <newemail>
2023-06-23 09:54:43 +0530

    added c

--- src/pages/A.java
+++ src/pages/A.java
@@ -1,5 +1,23 @@
 package pages;
 
-public class A {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map.Entry;
+import java.util.Set;
 
-}
+public class A
+{
+	public static void main(String[] args)
+	{
+		int fact=1;
+		for(int i=1;i<=5;i++)
+		{
+			fact=fact*i;
+		}
+		System.out.println(fact);
+	}
+	
+	}
+
--- src/pages/B.java
+++ src/pages/B.java
@@ -1,5 +1,16 @@
 package pages;
 
-public class B {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 
+public class B 
+{
+	
+	public static void main(String[] args) {
+	
+	}
+	
+	
 }
